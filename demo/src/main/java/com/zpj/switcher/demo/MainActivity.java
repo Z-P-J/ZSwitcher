17f720952417ed13bad8b36208be092b9c9d4db0
Z-P-J <37832345+Z-P-J@users.noreply.github.com>
2020-02-29 13:31:23 +0800

    重构代码

---
+++ demo/src/main/java/com/zpj/switcher/demo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.zpj.switcher.demo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.zpj.widget.switcher.CircleSwitcher;
+import com.zpj.widget.switcher.CommonSwitcher;
+import com.zpj.widget.switcher.OnCheckedChangeListener;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final int defaultColor = ContextCompat.getColor(this, R.color.text_color_default);
+        final int disabledColor = ContextCompat.getColor(this, R.color.text_color_disabled);
+
+        final TextView tvSwitcherX = findViewById(R.id.tv_switcher_x);
+        final TextView tvSwitcherC = findViewById(R.id.tv_switcher_c);
+        CommonSwitcher switcherX = findViewById(R.id.switcher_x);
+        final CircleSwitcher switcherC = findViewById(R.id.switcher_c);
+
+        switcherX.setOnCheckedChangeListener(checked -> {
+            switcherC.setChecked(checked);
+            tvSwitcherX.setTextColor(checked ? defaultColor : disabledColor);
+        });
+
+        switcherC.setOnCheckedChangeListener(checked -> tvSwitcherC.setTextColor(checked ? defaultColor : disabledColor));
+
+        findViewById(R.id.dribbble).setOnClickListener(v -> {
+            Uri uri = Uri.parse(getString(R.string.dribbble_link));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        });
+    }
+
+}
---
+++ library/src/main/java/com/zpj/utils/ScreenUtils.java
@@ -0,0 +1,106 @@
+package com.zpj.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
+public class ScreenUtils {
+
+    public static float dp2px(Context context, float dp) {
+        if (context == null) {
+            return -1;
+        }
+        return dp * density(context);
+    }
+
+    public static float px2dp(Context context, float px) {
+        if (context == null) {
+            return -1;
+        }
+        return px / density(context);
+    }
+
+    public static float density(Context context) {
+        return context.getResources().getDisplayMetrics().density;
+    }
+
+
+    public static int dp2pxInt(Context context, float dp) {
+        return (int) (dp2px(context, dp) + 0.5f);
+    }
+
+    public static float px2dpInt(Context context, float px) {
+        return (int) (px2dp(context, px) + 0.5f);
+    }
+
+
+    public static int sp2px(Context context, float spValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public static int px2sp(Context context, float pxValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    public static DisplayMetrics getDisplayMetrics(Context context) {
+        Activity activity;
+        if (!(context instanceof Activity) && context instanceof ContextWrapper) {
+            activity = (Activity) ((ContextWrapper) context).getBaseContext();
+        } else {
+            activity = (Activity) context;
+        }
+        DisplayMetrics metrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        return metrics;
+    }
+
+    /**
+     * 获取屏幕大小
+     *
+     * @param context
+     * @return
+     */
+    public static int[] getScreenPixelSize(Context context) {
+        DisplayMetrics metrics = getDisplayMetrics(context);
+        return new int[]{metrics.widthPixels, metrics.heightPixels};
+    }
+
+    public static int getScreenWidth(Context context) {
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight(Context context) {
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            return resources.getDimensionPixelSize(resourceId);
+        }
+        return 0;
+    }
+
+    /**
+     * 获取底部导航栏 (Navigation Bar) 高度
+     */
+    public static int getNavigationBarHeight(Context context) {
+        Resources resources = context.getResources();
+        int resourceId = context.getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            return resources.getDimensionPixelSize(resourceId);
+        }
+        return 0;
+    }
+
+    public static int getAppInScreenheight(Context context) {
+        return getScreenHeight(context) - getStatusBarHeight(context);
+    }
+
+
+}
